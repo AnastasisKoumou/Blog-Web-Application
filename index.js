3c65853c4023a2b0511c2d8d42b0cd47cf0f41d9
Anastasios Koumoundouros <anastasis7714@gmail.com>
2025-08-12 16:44:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 import express from "express";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
 
-let articleId = 1; 
-const articles = [];
+const DATA_FILE = "articles.json";
+
+let articles = [];
+let articleId = 1;
+
+if (fs.existsSync(DATA_FILE)) {
+  const fileData = fs.readFileSync(DATA_FILE, "utf-8");
+  if (fileData.trim()) {
+    articles = JSON.parse(fileData);
+    
+    articleId = articles.length ? Math.max(...articles.map(a => a.id)) + 1 : 1;
+  }
+}
+
+function saveArticles() {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(articles, null, 2), "utf-8");
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", (req,res) => {
-    res.render("index.ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get('/create', (req, res) => {
+app.get("/create", (req, res) => {
   res.render("create");
 });
 
@@ -22,13 +38,15 @@ app.post("/", (req, res) => {
   const { title, author, article } = req.body;
   const date = new Date().toLocaleString();
 
-  articles.push({ 
+  articles.push({
     id: articleId++,
-    title, 
-    author, 
-    article, 
-    date 
+    title,
+    author,
+    article,
+    date
   });
+
+  saveArticles(); 
   res.redirect("/view?success=true");
 });
 
@@ -37,7 +55,7 @@ app.get("/view", (req, res) => {
   res.render("view", { articles, success });
 });
 
-app.get('/edit', (req, res) => {
+app.get("/edit", (req, res) => {
   const success = req.query.success === "true";
   res.render("edit", { articles, success });
 });
@@ -62,6 +80,7 @@ app.post("/update/:id", (req, res) => {
   article.article = req.body.article;
   article.date = new Date().toLocaleString();
 
+  saveArticles();
   res.redirect("/edit?success=true");
 });
 
@@ -71,11 +90,12 @@ app.post("/delete/:id", (req, res) => {
 
   if (index !== -1) {
     articles.splice(index, 1);
+    saveArticles();
   }
 
   res.redirect("/edit?success=true");
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
